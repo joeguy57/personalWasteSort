3a39f77524705b3a3c76eda3ca390bee5f60118e
jwmatson <matsonj2013@gmail.com>
2020-01-09 13:57:12 -0700

    Created bin class, gave it color and information attributes, as long as created getters to retrieve this information.

---
+++ app/src/main/java/com/example/wastesortapp/Bin.java
@@ -0,0 +1,25 @@
+package com.example.wastesortapp;
+
+import android.graphics.Color;
+import android.icu.text.IDNA.Info;
+
+/**
+ * The bin class represents the colored bins within the gameActivity. They will be
+ * when a bin has an item dragged over it, will access a unique function which will check an array
+ * to see if the information matches
+ */
+public class Bin {
+  private String color = "";
+  private String info = "";
+
+  public Bin(String color, String info){
+    this.color = color;
+    this.info = info;
+  }//Bin
+  public String getColor(){
+    return color;
+  }//getColor
+  public String getInfo(){
+    return info;
+  }//getInfo
+}
