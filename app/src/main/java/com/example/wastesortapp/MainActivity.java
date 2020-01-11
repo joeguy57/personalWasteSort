33f5bebd59eafa9f34ea7b875f92f531e735525d
Harshil-V <harshil@ualberta.ca>
2020-01-11 12:49:23 -0700

    I fixed can not retrive image from the firebase database

--- app/src/main/java/com/example/wastesortapp/MainActivity.java
+++ app/src/main/java/com/example/wastesortapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.view.animation.DecelerateInterpolator;
 
--- app/src/main/java/com/example/wastesortapp/game.java
+++ app/src/main/java/com/example/wastesortapp/game.java
@@ -1,12 +1,17 @@
 package com.example.wastesortapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class game extends AppCompatActivity {
     ImageView imageView;
@@ -19,6 +24,23 @@ public class game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        imageView = findViewById(R.id.imageGet);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        first.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String link = dataSnapshot.getValue(String.class);
+                Picasso.get().load(link).into(imageView);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
