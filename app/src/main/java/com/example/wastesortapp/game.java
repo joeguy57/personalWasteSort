23219ba533be6b4073b04a0c96495742e3ff981e
Harshil-V <harshil@ualberta.ca>
2020-01-10 22:28:55 -0700

    connected to firebase and made the mainactivity locked in portrait

--- app/src/main/java/com/example/wastesortapp/game.java
+++ app/src/main/java/com/example/wastesortapp/game.java
@@ -3,12 +3,22 @@ package com.example.wastesortapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class game extends AppCompatActivity {
+    ImageView imageView;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference= firebaseDatabase.getReference();
+    private DatabaseReference first = databaseReference.child("image");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+
     }
 }
