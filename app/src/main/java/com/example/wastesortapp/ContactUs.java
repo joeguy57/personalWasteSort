c9a1d53b96fc8b89b3b8600b79ffeff5bd2616f1
joeguy57 <56702275+joeguy57@users.noreply.github.com>
2020-01-12 13:45:06 -0700

    Created Image that will send user to the url link.

--- app/src/main/java/com/example/wastesortapp/ContactUs.java
+++ app/src/main/java/com/example/wastesortapp/ContactUs.java
@@ -2,23 +2,32 @@ package com.example.wastesortapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class ContactUs extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_us);
-        ImageView backBtn = findViewById(R.id.backBtn);
-
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact_us);
+		ImageView backBtn = findViewById(R.id.backBtn);
+		
+		backBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
+	
+	public void goToBrowser(View view) {
+		Intent browserAccess = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ualberta" +
+        ".ca/augustana/about-us/departments-offices-and-units/lab/sustainability/campus" +
+        "-initiatives/waste"));
+		startActivity(browserAccess);
+	}
 }
