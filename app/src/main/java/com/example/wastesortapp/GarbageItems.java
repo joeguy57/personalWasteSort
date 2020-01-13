99960ab313a78474fa562375943d6e13ee834f9e
joeguy57 <56702275+joeguy57@users.noreply.github.com>
2020-01-12 21:37:38 -0700

    Databases is now working, but cant retrieve from another class, will fix for later.

--- app/src/main/java/com/example/wastesortapp/GarbageItems.java
+++ app/src/main/java/com/example/wastesortapp/GarbageItems.java
@@ -1,80 +1,83 @@
 package com.example.wastesortapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 
+import androidx.annotation.Nullable;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import java.util.Map;
 
 public class GarbageItems extends game {
-	private final DatabaseReference gameObjectRef;
-	private FirebaseDatabase database;
-	private DatabaseReference objectRef;
-	private DatabaseReference DataRef;
-	
-	public String Color;
-	public String itemName;
-	
-	public GarbageItems(int itemNum) {
-		String itemId = String.valueOf(itemNum);
-		database = FirebaseDatabase.getInstance();
-		objectRef = database.getReference().child("1WTVDXleXTbtGu43obhTU9fwozWAtG0R1Cw464U3mvlk");
-		gameObjectRef =  objectRef.child("gameObjects");
-		
-		DataRef = gameObjectRef.child(itemId);
-		DatabaseReference colorDatabase = DataRef.child("Color");
-		
-		colorGrabber(colorDatabase);
-		DatabaseReference itemNameDatabase = DataRef.child("Item");
-		itemGrabber(itemNameDatabase);
-		
-	}//GarbageItems(Constructor)
-	
-	
-	
-	public String getColor() {
-		return Color;
-	}
-	public void setColor(String color) {
-		this.Color = color;
-	}
-	public String getItemName() {
-		return itemName;
-	}
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-		
-	}
-	
-	private void colorGrabber(DatabaseReference colorDatabase){
-		colorDatabase.addValueEventListener(new ValueEventListener() {
-			@Override
-			public void onDataChange( DataSnapshot dataSnapshot) {
-				String Colors = dataSnapshot.getValue().toString();
-				setColor(Colors);
-			}
-			@Override
-			public void onCancelled(@NonNull DatabaseError databaseError) {
-			}
-		});
-	}
-	private void itemGrabber(DatabaseReference itemNameDatabase) {
-		itemNameDatabase.addValueEventListener(new ValueEventListener() {
-			@Override
-			public void onDataChange( DataSnapshot dataSnapshot) {
-				String itemName = dataSnapshot.getValue().toString();
-				setItemName(itemName);
-			}
-			@Override
-			public void onCancelled(@NonNull DatabaseError databaseError) {
-			}
-		});
-	}
-	
+
+  private final DatabaseReference gameObjectRef;
+  private FirebaseDatabase database;
+
+  public String Color;
+  public String itemName;
+
+  public GarbageItems(int itemNum) {
+    final String itemId = String.valueOf(itemNum);
+
+    database = FirebaseDatabase.getInstance();
+    gameObjectRef = database.getReference().child("gameObjects");
+    itemInformationGrabber(gameObjectRef, itemId);
+
+  }//GarbageItems(Constructor)
+
+
+  public String getColor() {
+    return Color;
+  }
+
+  public void setColor(String color) {
+    this.Color = color;
+  }
+
+  public String getItemName() {
+    return itemName;
+  }
+
+  public void setItemName(String itemName) {
+    this.itemName = itemName;
+
+  }
+
+  private void itemInformationGrabber(DatabaseReference gameObjectRef, String itemId) {
+    DatabaseReference dataReference = gameObjectRef.child(itemId);
+    DatabaseReference itemColor = dataReference.child("Color");
+    DatabaseReference itemNames = dataReference.child("Item");
+    itemColor.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange( DataSnapshot dataSnapshot) {
+        String color = dataSnapshot.getValue(String.class);
+        setColor(color);
+      }
+      @Override
+      public void onCancelled(@NonNull DatabaseError databaseError) {
+      }
+    });
+    itemNames.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        String name = dataSnapshot.getValue(String.class);
+        setItemName(name);
+      }
+      @Override
+      public void onCancelled(@NonNull DatabaseError databaseError) {
+      }
+    });
+
+  }
+
+
+
 }//GarbageItems
--- app/src/main/java/com/example/wastesortapp/game.java
+++ app/src/main/java/com/example/wastesortapp/game.java
@@ -1,24 +1,88 @@
 package com.example.wastesortapp;
 
+import android.os.Build.VERSION_CODES;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.concurrent.CompletableFuture;
 
 public class game extends AppCompatActivity {
-    TextView colorTextView;
-    TextView itemTextView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-        colorTextView = findViewById(R.id.ColorTextView);
-        itemTextView = findViewById(R.id.ItemNameTextView);
-        
-        GarbageItems items = new GarbageItems(1);
-        
-        colorTextView.setText(items.Color);
-        itemTextView.setText(items.itemName);
-        
-    }
+
+  TextView colorTextView;
+  TextView itemTextView;
+  private String Color;
+  private String itemName;
+
+  @RequiresApi(api = VERSION_CODES.N)
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_game);
+    colorTextView = findViewById(R.id.ColorTextView);
+    itemTextView = findViewById(R.id.ItemNameTextView);
+
+    //GarbageItems garbageItems = new GarbageItems(1);
+    //Color =  garbageItems.getColor();
+
+    itemInformationGrabber("1");
+  }
+
+  public String getColor() {
+    return Color;
+  }
+
+  public void setColor(String color) {
+    this.Color = color;
+  }
+
+  public String getItemName() {
+    return itemName;
+  }
+
+  public void setItemName(String itemName) {
+    this.itemName = itemName;
+
+  }
+
+  private void itemInformationGrabber(String itemId) {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference gameObjectRef = database.getReference().child("gameObjects");
+
+    DatabaseReference dataReference = gameObjectRef.child(itemId);
+
+    DatabaseReference itemColor = dataReference.child("Color");
+    DatabaseReference itemNames = dataReference.child("Item");
+
+    itemColor.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange( DataSnapshot dataSnapshot) {
+        String color = dataSnapshot.getValue(String.class);
+        setColor(color);
+      }
+      @Override
+      public void onCancelled(@NonNull DatabaseError databaseError) {
+      }
+    });
+
+    itemNames.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        String name = dataSnapshot.getValue(String.class);
+        setItemName(name);
+      }
+      @Override
+      public void onCancelled(@NonNull DatabaseError databaseError) {
+      }
+    });
+  }
+
+
 }
