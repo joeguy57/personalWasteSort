10b8d95e212b09a96c9ba72222296257d33fae36
Harshil-V <harshil@ualberta.ca>
2020-01-12 19:33:23 -0700

    merged with Harshil branch

--- app/src/main/java/com/example/wastesortapp/MainActivity.java
+++ app/src/main/java/com/example/wastesortapp/MainActivity.java
@@ -10,47 +10,46 @@ import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        final Button gameBtn = findViewById(R.id.gameBtn);
-        Button disposableHelpBtn = findViewById(R.id.disposableHelpBtn);
-        Button moreInfo = findViewById(R.id.moreInfoBtn);
+    final Button gameBtn = findViewById(R.id.gameBtn);
+    Button disposableHelpBtn = findViewById(R.id.disposableHelpBtn);
+    Button moreInfo = findViewById(R.id.moreInfoBtn);
 
+    gameBtn.setOnClickListener(new CompoundButton.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        gameBtn.setEnabled(true);
 
-        gameBtn.setOnClickListener(new CompoundButton.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gameBtn.setEnabled(true);
+      }
+    });
 
-            }
-        });
+    gameBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent goToActivity = new Intent(getApplicationContext(), game.class);
+        startActivity(goToActivity);
+      }
+    });
 
-       gameBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent goToActivity = new Intent(getApplicationContext(), game.class);
-               startActivity(goToActivity);
-           }
-       });
+    disposableHelpBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent goToActivity = new Intent(getApplicationContext(), DisposableHelp.class);
+        startActivity(goToActivity);
+      }
+    });
 
-       disposableHelpBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent goToActivity = new Intent(getApplicationContext(), DisposableHelp.class);
-               startActivity(goToActivity);
-           }
-       });
+    moreInfo.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent goToActivity = new Intent(getApplicationContext(), ContactUs.class);
+        startActivity(goToActivity);
+      }
+    });
 
-        moreInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToActivity = new Intent(getApplicationContext(), ContactUs.class);
-                startActivity(goToActivity);
-            }
-        });
-
-    }
+  }
 }
