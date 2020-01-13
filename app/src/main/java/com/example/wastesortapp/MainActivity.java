0d35086eda5d21e38fa864cbb5357685bf6cff33
Harshil-V <harshil@ualberta.ca>
2020-01-12 19:32:11 -0700

    Deleted any unused code

--- app/src/main/java/com/example/wastesortapp/MainActivity.java
+++ app/src/main/java/com/example/wastesortapp/MainActivity.java
@@ -6,51 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected Intent goToActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button gameBtn = findViewById(R.id.gameBtn);
+        Button gameBtn = findViewById(R.id.gameBtn);
         Button disposableHelpBtn = findViewById(R.id.disposableHelpBtn);
         Button moreInfo = findViewById(R.id.moreInfoBtn);
 
-
-        gameBtn.setOnClickListener(new CompoundButton.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gameBtn.setEnabled(true);
-
-            }
-        });
-
-       gameBtn.setOnClickListener(new View.OnClickListener() {
+        gameBtn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Intent goToActivity = new Intent(getApplicationContext(), game.class);
-               startActivity(goToActivity);
+               startActivity(goToActivity = new Intent(getApplicationContext(), game.class));
+
            }
        });
 
        disposableHelpBtn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Intent goToActivity = new Intent(getApplicationContext(), DisposableHelp.class);
-               startActivity(goToActivity);
+               startActivity(goToActivity = new Intent(getApplicationContext(), DisposableHelp.class));
+
            }
        });
 
         moreInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToActivity = new Intent(getApplicationContext(), ContactUs.class);
-                startActivity(goToActivity);
+                startActivity(goToActivity = new Intent(getApplicationContext(), ContactUs.class));
+
             }
         });
 
     }
+
+
 }
