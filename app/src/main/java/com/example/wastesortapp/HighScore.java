a477eb4c6a49947c0e47d8041b7283a8b5dd27b8
joeguy57 <56702275+joeguy57@users.noreply.github.com>
2020-01-17 12:57:35 -0700

    Highscore sending data through database and connecting to google sheets, Done! takes ~5 min to  sync database with google sheets.

--- app/src/main/java/com/example/wastesortapp/HighScore.java
+++ app/src/main/java/com/example/wastesortapp/HighScore.java
@@ -1,11 +1,12 @@
 /*
-* HighScore.java
-* This Activity sends the data to the firebase for saved Data.
-* Here score will be sent from the game.java and request (but not require),
-* the user to input either their name or email.
-* */
+ * HighScore.java
+ * This Activity sends the data to the firebase for saved Data.
+ * Here score will be sent from the game.java and request (but not require),
+ * the user to input either their name or email.
+ * */
 package com.example.wastesortapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -24,6 +25,7 @@ public class HighScore extends AppCompatActivity {
   private EditText emailTextView;
   private Boolean emailValid;
   private String emailInput;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -44,34 +46,59 @@ public class HighScore extends AppCompatActivity {
     int emailStartIndex = emailInput.indexOf('@');
     String emailSubString = emailInput.substring(emailStartIndex + 1);
 
-    if (emailSubString.equals(emailPref)){
+    if (emailSubString.equals(emailPref)) {
       return true;
     }
     return false;
   }
 
-  public void sendData(View view){
+  public void sendData(View view) {
+    if (nameInput.getText().length() == 0 && emailTextView.getText().length() == 0) {
+      Intent goToActivity;
+      startActivity(goToActivity = new Intent(getApplicationContext(), MainActivity.class));
+    }//if
+    else if (emailTextView.getText().length() == 0) {
+      String key;
+      DatabaseReference writeData = rootRef.child("1WTVDXleXTbtGu43obhTU9fwozWAtG0R1Cw464U3mvlk")
+          .child("HighScore");
+      if (writeData.getKey() != String.valueOf(1)) {
+        key = writeData.push().getKey();
+      } else {
+        key = "1";
+      }
+      SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy 'at' HH:mm:ss z");
+      Date date = new Date(System.currentTimeMillis());
+      writeData.child(key).child("Date").setValue(formatter.format(date));
+      writeData.child(key).child("Score").setValue(score);
+      writeData.child(key).child("Name").setValue(nameInput.getText().toString());
+      Intent goToActivity;
+      startActivity(goToActivity = new Intent(getApplicationContext(), MainActivity.class));
+    }
+    if (checkEmail(emailTextView)) {
+      String key;
+      DatabaseReference writeData = rootRef.child("1WTVDXleXTbtGu43obhTU9fwozWAtG0R1Cw464U3mvlk")
+          .child("HighScore");
+      if (writeData.getKey() != String.valueOf(1)) {
+        key = writeData.push().getKey();
+      } else {
+        key = "1";
+      }
+      SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy 'at' HH:mm:ss z");
+      Date date = new Date(System.currentTimeMillis());
+      writeData.child(key).child("Date").setValue(formatter.format(date));
+      writeData.child(key).child("Score").setValue(score);
+      writeData.child(key).child("Name").setValue(nameInput.getText().toString());
 
-    String key;
-    DatabaseReference writeData = rootRef.child("1WTVDXleXTbtGu43obhTU9fwozWAtG0R1Cw464U3mvlk").child("HighScore");
-    if (writeData.getKey() != String.valueOf(1)) {
-      key = writeData.push().getKey();
+      writeData.child(key).child("emailAddress").setValue(emailInput);
+      Intent goToActivity;
+      startActivity(goToActivity = new Intent(getApplicationContext(), MainActivity.class));
+
+    } else {
+      emailTextView.setText("");
+      emailTextView.setError("Please use ualberta email");
+
     }
-    else{
-      key = "1";
-    }
-    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy 'at' HH:mm:ss z" );
-    Date date = new Date(System.currentTimeMillis());
-    writeData.child(key).child("Date").setValue(formatter.format(date));
-    writeData.child(key).child("Score").setValue(score);
-    writeData.child(key).child("Name").setValue(nameInput.getText().toString());
-    if(!emailInput.isEmpty()) {
-      emailValid = checkEmail(emailTextView);
-      while (!emailValid) {
-        emailTextView.setError("Invalid Email: please use your ualberta email address");
-      }//if
-      writeData.child(key).child("Email").setValue(emailInput);
-    }//outter if
+
 
   }//sendDataa
 }//HighScore
