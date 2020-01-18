96d9aadaabd6c417b7e4642ba3ad1b73a0abc5e5
joeguy57 <56702275+joeguy57@users.noreply.github.com>
2020-01-18 15:50:22 -0700

    Rearranged the wasteGuide, currently working on image insertion will have to rebuild project to work properly.

--- app/src/main/java/com/example/wastesortapp/Adapter.java
+++ app/src/main/java/com/example/wastesortapp/Adapter.java
@@ -3,41 +3,65 @@ package com.example.wastesortapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder>{
-ArrayList<GarbageInfo> list;
-  public Adapter(ArrayList<GarbageInfo> list) {
+public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder> {
+
+  private ArrayList<GarbageInfo> list;
+
+  Adapter(ArrayList<GarbageInfo> list) {
     this.list = list;
   }
 
   @NonNull
   @Override
   public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view,parent, false);
+    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view, parent, false);
     return new viewHolder(view);
   }
 
   @Override
   public void onBindViewHolder(@NonNull viewHolder holder, int position) {
     holder.nameView.setText(list.get(position).getName());
-    holder.colorView.setText(list.get(position).getColor());
+    holder.colorView.setText(list.get(position).getBinType());
+    holder.descriptionView.setText(list.get(position).getDescription());
+    switch (list.get(position).getBinType()) {
+      case "Others":
+        //holder.imageView.setImageDrawable();
+        break;
+      case "Trash":
+        break;
+      case "Recyclables":
+        break;
+      case "Beverages":
+        break;
+      case "Organics":
+        break;
+      default:
+        break;
+    }
   }
 
   @Override
   public int getItemCount() {
-    return list.size();
+    return (list.size());
   }
 
-  class viewHolder extends RecyclerView.ViewHolder{
-TextView nameView, colorView;
+  class viewHolder extends RecyclerView.ViewHolder {
+
+    TextView nameView, colorView, descriptionView;
+    ImageView imageView;
+
     public viewHolder(@NonNull View itemView) {
       super(itemView);
       nameView = itemView.findViewById(R.id.itemName);
-      colorView = itemView.findViewById(R.id.itemDescripton);
+      colorView = itemView.findViewById(R.id.itemBinType);
+      descriptionView = itemView.findViewById(R.id.itemDescription);
+      imageView = itemView.findViewById(R.id.binImage);
     }
   }
 }
