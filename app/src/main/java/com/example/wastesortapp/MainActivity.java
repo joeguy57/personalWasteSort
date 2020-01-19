7175da1697acb28db5b48885c8db3712b3de2167
Harshil-V <harshil@ualberta.ca>
2020-01-19 12:07:50 -0700

    create a class to make objects fade in and made the main menu logo fade in

--- app/src/main/java/com/example/wastesortapp/MainActivity.java
+++ app/src/main/java/com/example/wastesortapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AlertDialog.Builder;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,19 +18,24 @@ public class MainActivity extends AppCompatActivity {
 
     protected Intent goToActivity;
     Animation fromBottom;
+    Animation fadeIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
+        ImageView mainMenuLogo = findViewById(R.id.mainMenuLogo);
         Button gameBtn = findViewById(R.id.gameBtn);
         Button disposableHelpBtn = findViewById(R.id.disposableHelpBtn);
         Button moreInfo = findViewById(R.id.moreInfoBtn);
 
         fromBottom = AnimationUtils.loadAnimation(this,R.anim.from_bottom);
+        fadeIn = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+
         gameBtn.setAnimation(fromBottom);
         disposableHelpBtn.setAnimation(fromBottom);
         moreInfo.setAnimation(fromBottom);
+        mainMenuLogo.setAnimation(fadeIn);
 
 
         gameBtn.setOnClickListener(new View.OnClickListener() {
